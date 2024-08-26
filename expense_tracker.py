04bcc32f56de8bcd7f2341a5b77b9867c4e726e9
PonePone23 <myopone@gmail.com>
2024-08-26 23:30:29 +0630

    add daily budget

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,3 +1,5 @@
+import calendar
+import datetime
 from expense import Expense
 
 
@@ -90,6 +92,17 @@ def summarize_expenses(expense_file_path,budget):
     remaining_budget = budget- total_spent
     print(f"Budget Remaining: ${remaining_budget:.2f}")
 
+    now = datetime.datetime.now()
+    days_in_month = calendar.monthrange(now.year,now.month)[1]
+    remaining_days = days_in_month - now.day
+    print("Remaining days in the courrent month:",remaining_days)
+
+    daily_budget = remaining_budget / remaining_days
+    print(green(f"Budget per day ${daily_budget}"))
+    
+def green(text):
+    return f"\033[92m{text}\033[0m"
+    
 
 if __name__ == "__main__":
     main()
